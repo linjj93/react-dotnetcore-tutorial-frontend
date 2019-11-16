4a2e34a8e1e355ec2fb6d4c09419602aa7a88908
linjj93 <linjj93@hotmail.com>
2019-11-16 21:22:02 +0800

    Add Semantic UI and fetch data from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
+import "./App.css";
 
 const App: React.FC = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/values").then(res => {
+      setData(res.data);
+    });
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as="h2">
+        <Icon name="plug" />
+        <Header.Content>Uptime Guarantee</Header.Content>
+      </Header>
+      <List>
+        {data.map((value: any) => (
+          <List.Item key={value.id}>{value.name}</List.Item>
+        ))}
+      </List>
     </div>
   );
-}
+};
 
 export default App;
